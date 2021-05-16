16434827ad8643b4e4e996faae214f96ca0ee917
napaulas <flekpaula@gmail.com>
2021-05-16 17:03:16 -0300

    modificações js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-alert('Olá!');
+const original = document.querySelector('#original')
+const contador = document.querySelector('#contador')
+const modificado = document.querySelector('#modificado')
+const efeito = document.querySelector('#efeito')
+original.addEventListener("keyup", function () {
+    console.log(original.value.length)
+    contador.innerHTML = 0 + original.value.length
+
+    if (efeito.value == "uppercase") {
+        modificado.value = original.value.toUpperCase()
+    } else {
+        modificado.value = original.value.toLowerCase()
+    }
+
+})
+
+
+
+efeito.addEventListener("change", function () {
+    if (efeito.value == "uppercase") {
+        modificado.value = original.value.toUpperCase()
+    } else {
+        modificado.value = original.value.toLowerCase()
+    }
+})
